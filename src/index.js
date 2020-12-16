5c16e9aa84c812b2e1730729a6264f451d78bb06
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-16 09:54:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,21 +51,15 @@ app.get('/hotspotStates',(req,res)=>{
 
 
 
-// /healthyStates
-
-app.get('/hotspotStates',(req,res)=>{
-    connection.aggregate([
-        { $match: {} },
-        { $group: { _id: "total", death: { $sum: "$death" } } }
-     ]).then((ele)=>res.send({"data":ele[0]}));
-});
-
 app.get('/healthyStates',(req,res)=>{
-    connection.aggregate([
-        { $match: {} },
-        { $group: { _id: "total", death: { $sum: "$death" } } }
-     ]).then((ele)=>res.send({"data":ele[0]}));
+    connection.update({
+        $set:{mortality:{$round : [ {$divide:[{$subtract:["$infected","$recovered"]},"$infected"]}, 5 ]}}
+   }).find({mortality:{$gt:0.1}}).then((result)=>{
+       res.send(result);
+   }).catch(err=>console.log(err.message));
 });
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
